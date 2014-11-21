d78cc66d024b9ec55b07cf4a12a380b66d6296ab
WebFreak001 <janju007@web.de>
2014-11-21 21:16:22 +0100

    Removed german comments, added vec3(vec2, z)

--- EncoShared/EncoShared.h
+++ EncoShared/EncoShared.h
@@ -1,11 +1,6 @@
-#ifndef __ENCOSHARED_ENCOSHARED_H__
-#define __ENCOSHARED_ENCOSHARED_H__
-
 #pragma once
 
 #include "stdafx.h"
 #include "Vector3.h"
 #include "IView.h"
 #include "Vector2.h"
-
-#endif
