d3bdba82842e82c7cb9847d050284ab4d34552d8
NaroncoNexo <johannes.roth@aol.com>
2014-11-21 20:36:58 +0100

    Removed ALL

--- Enco3D/Enco3D/Input.cpp
+++
@@ -1,9 +0,0 @@
-#include "Input.h"
-
-bool *Enco3D::Core::Input::m_keys = nullptr;
-bool *Enco3D::Core::Input::m_mouseButtons = nullptr;
-
-int Enco3D::Core::Input::m_mouseX = 0;
-int Enco3D::Core::Input::m_mouseY = 0;
-int Enco3D::Core::Input::m_relativeX = 0;
-int Enco3D::Core::Input::m_relativeY = 0;
