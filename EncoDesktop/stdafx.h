90718d094f58a9d1ebb5487170021e711ed2fced
NaroncoNexo <johannes.roth@aol.com>
2014-11-22 10:18:00 +0100

    Moved SDL2 include to stdafx.h (EncoDesktop)

--- EncoDesktop/stdafx.h
+++ EncoDesktop/stdafx.h
@@ -7,6 +7,9 @@
 #	define WIN32_LEAN_AND_MEAN
 #	include <windows.h>
 
+#	include <SDL2\SDL.h>
+#	pragma comment (lib, "SDL2.lib")
+
 #	ifdef ENCODESKTOP_EXPORTS
 #		define ENCODESKTOPAPI __declspec(dllexport)
 #	else
