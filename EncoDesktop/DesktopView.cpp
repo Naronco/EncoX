dcb7937d3781d8d03524376a0e543a9b1e3f83b7
NaroncoNexo <johannes.roth@aol.com>
2014-11-22 11:10:28 +0100

    Added OpenGL support for DesktopView

--- EncoDesktop/DesktopView.cpp
+++ EncoDesktop/DesktopView.cpp
@@ -3,20 +3,33 @@
 
 namespace enco {
 	ENCODESKTOPAPI void DesktopView::create() {
-		SDL_Init(SDL_INIT_EVERYTHING);
+		SDL_Init(SDL_INIT_VIDEO);
 
-		m_window = SDL_CreateWindow("EncoX", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, (int)m_size.x, (int)m_size.y, SDL_WINDOW_SHOWN);
+		m_window = SDL_CreateWindow("EncoX", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, (int)m_size.x, (int)m_size.y, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
 		if (!m_window) {
 			// TODO: Error handling
+			return;
+		}
+
+		if (glewInit() == GL_FALSE) {
+#ifdef _DEBUG
+			printf("GLEW Error: Initialization\n");
+#endif
 			return;
 		}
 	}
 
 	ENCODESKTOPAPI void DesktopView::destroy() {
+		if (m_openGLContext) {
+			m_openGLContext.reset();
+		}
+
 		if (m_window) {
 			SDL_DestroyWindow(m_window);
 			m_window = nullptr;
 		}
+
+		SDL_Quit();
 	}
 
 	ENCODESKTOPAPI void DesktopView::update(float deltaTime) {
@@ -28,9 +41,12 @@ namespace enco {
 				break;
 			}
 		}
+
+		SDL_GL_SwapWindow(m_window);
 	}
 
 	ENCODESKTOPAPI void DesktopView::createOpenGLContext(const OpenGLContextParameters &parameters) {
-		// TODO: Create Desktop OpenGLContext
+		m_openGLContext = std::shared_ptr<DesktopOpenGLContext>(new DesktopOpenGLContext(m_window, parameters));
+		m_openGLContext->makeCurrent();
 	}
 }
--- EncoDesktop/EncoDesktop.h
+++ EncoDesktop/EncoDesktop.h
@@ -6,5 +6,6 @@
 #include "stdafx.h"
 
 #include "DesktopView.h"
+#include "DesktopOpenGLContext.h"
 
 #endif
