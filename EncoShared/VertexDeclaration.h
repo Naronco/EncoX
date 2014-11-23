73c4f6db02fb4509c4aaa28e42b77de4ddfede61
WebFreak001 <janju007@web.de>
2014-11-23 12:32:40 +0100

    Completed abstract Vertex classes (EncoShared)

--- EncoShared/VertexDeclaration.h
+++ EncoShared/VertexDeclaration.h
@@ -5,17 +5,50 @@
 
 #include "stdafx.h"
 
-namespace enco {
-	struct VertexDeclaration {
+#include <vector>
 
+namespace enco {
+	struct VertexElement {
+		VertexElement(i32 offset, VertexElementFormat format, VertexElementUsage usage, i32 usageIndex) : offset(offset), format(format), usage(usage), usageIndex(usageIndex) { }
+
+		i32 offset, usageIndex;
+		VertexElementFormat format;
+		VertexElementUsage usage;
+	};
+
+	class VertexDeclaration {
+	public:
+		VertexDeclaration(std::vector<VertexElement> elems) : m_elements(elems) {}
+		VertexDeclaration() : VertexDeclaration(std::vector<VertexElement>()) {}
+		~VertexDeclaration() { m_elements.clear(); }
+
+		inline void addElement(VertexElement element) { m_elements.push_back(element); }
+		inline std::vector<VertexElement> getElements() { return m_elements; }
+
+	private:
+		std::vector<VertexElement> m_elements;
+	};
+
+	enum VertexElementFormat {
+		vec1,
+		vec2,
+		vec3,
+		vec4,
+		normalizedVec1,
+		normalizedVec2,
+		normalizedVec3,
+		normalizedVec4,
+		/// <summary>Four-component packed int32 (ARGB)</summary>
+		color
 	};
 
 	enum VertexElementUsage {
 		position // TODO: textureCoordinate, normal, color, etc.
 	};
 
-	struct VertexElement {
-
+	class IVertexType {
+	public:
+		virtual VertexDeclaration* getVertexDeclaration() = 0;
 	};
 }
 
