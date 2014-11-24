f615b635f4ff1182fa283851a74e4bfa1115e9ca
Johannes Roth <johannes.roth@aol.com>
2014-11-24 17:04:54 +0100

    Started working for support of iOS

---
+++ EncoX_ios/EncoShared/EncoShared/EncoShared.h
@@ -0,0 +1,13 @@
+//
+//  EncoShared.h
+//  EncoShared
+//
+//  Created by Johannes Roth on 24.11.14.
+//  Copyright (c) 2014 Naronco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EncoShared : NSObject
+
+@end
---
+++ EncoX_ios/EncoShared/EncoShared/EncoShared.m
@@ -0,0 +1,13 @@
+//
+//  EncoShared.m
+//  EncoShared
+//
+//  Created by Johannes Roth on 24.11.14.
+//  Copyright (c) 2014 Naronco. All rights reserved.
+//
+
+#import "EncoShared.h"
+
+@implementation EncoShared
+
+@end
